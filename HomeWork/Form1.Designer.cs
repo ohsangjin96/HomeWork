5270ce1e6b9a0d6dbc532fc3b0d830c750bea8e6
한승희 <tmdgml2519@naver.com>
2020-10-12 09:51:45 +0900

    초기 커밋.

---
+++ HomeWork/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace HomeWork
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.난이도ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.초급ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.중급ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.고급ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("궁서", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.groupBox1.Location = new System.Drawing.Point(107, 78);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "점수";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label1.Location = new System.Drawing.Point(98, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Font = new System.Drawing.Font("궁서", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.groupBox2.Location = new System.Drawing.Point(423, 78);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 100);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "남은 횟수";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label2.Location = new System.Drawing.Point(98, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 24);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "label2";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Enabled = false;
+            this.progressBar1.Location = new System.Drawing.Point(107, 236);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(516, 23);
+            this.progressBar1.TabIndex = 2;
+            this.progressBar1.Value = 100;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("궁서", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.textBox1.Location = new System.Drawing.Point(107, 334);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(516, 29);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("궁서", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label3.Location = new System.Drawing.Point(107, 284);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 24);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "label3";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.난이도ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 난이도ToolStripMenuItem
+            // 
+            this.난이도ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.초급ToolStripMenuItem,
+            this.중급ToolStripMenuItem,
+            this.고급ToolStripMenuItem});
+            this.난이도ToolStripMenuItem.Name = "난이도ToolStripMenuItem";
+            this.난이도ToolStripMenuItem.Size = new System.Drawing.Size(55, 20);
+            this.난이도ToolStripMenuItem.Text = "난이도";
+            // 
+            // 초급ToolStripMenuItem
+            // 
+            this.초급ToolStripMenuItem.Name = "초급ToolStripMenuItem";
+            this.초급ToolStripMenuItem.Size = new System.Drawing.Size(98, 22);
+            this.초급ToolStripMenuItem.Text = "초급";
+            this.초급ToolStripMenuItem.Click += new System.EventHandler(this.초급ToolStripMenuItem_Click);
+            // 
+            // 중급ToolStripMenuItem
+            // 
+            this.중급ToolStripMenuItem.Name = "중급ToolStripMenuItem";
+            this.중급ToolStripMenuItem.Size = new System.Drawing.Size(98, 22);
+            this.중급ToolStripMenuItem.Text = "중급";
+            this.중급ToolStripMenuItem.Click += new System.EventHandler(this.중급ToolStripMenuItem_Click);
+            // 
+            // 고급ToolStripMenuItem
+            // 
+            this.고급ToolStripMenuItem.Name = "고급ToolStripMenuItem";
+            this.고급ToolStripMenuItem.Size = new System.Drawing.Size(98, 22);
+            this.고급ToolStripMenuItem.Text = "고급";
+            this.고급ToolStripMenuItem.Click += new System.EventHandler(this.고급ToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 난이도ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 초급ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 중급ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 고급ToolStripMenuItem;
+    }
+}
+
---
+++ HomeWork/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+
+namespace HomeWork
+{
+    public partial class Form1 : Form
+    {
+        int point=0;
+        int life=10;
+        int level;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            label1.Text = point.ToString();
+            label2.Text = life.ToString();
+            timer1.Interval = 500;
+            label3.Text ="난이도를 먼저 선택해주세요";
+            
+        }
+
+        private string File()
+        {
+            
+            List<string> list = new List<string>();
+            Random rand = new Random();
+            using (StreamReader sr = new StreamReader("level1.txt"))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    list.Add(line);
+                   
+                }
+            }
+
+            int a = rand.Next(0, list.Count);
+            string[] a_sr = list.ToArray();
+           
+            return a_sr[a];
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+           
+            if (progressBar1.Value < 10)
+            {
+                life = life - 1;
+                label2.Text = life.ToString();
+                progressBar1.Value = 100;
+                timer1.Stop();
+            }
+            else
+                progressBar1.Value = progressBar1.Value - level;
+
+           
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //e.KeyChar//캐릭 값
+               if(e.KeyChar==13)//13이 엔터를 뜻함
+            {
+               
+                if (textBox1.Text == label3.Text)
+                {
+                    point = point + 100;
+                    label1.Text = point.ToString();
+                   
+                    label3.Text = File();
+                    textBox1.Text = "";
+                    timer1.Stop();
+                }
+                else
+                {
+                    life = life - 1;
+                    label2.Text = life.ToString();
+                    
+                    label3.Text = File();
+                    textBox1.Text = "";
+                    if (life == 0)
+                    {
+                        MessageBox.Show("목숨이 다 됬습니다.");
+                        Close();
+                    }
+                    timer1.Stop();
+                }
+                progressBar1.Value = 100;
+            }
+          
+
+        }
+
+        private void 초급ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            label3.Text = File();
+            level = 10;
+        }
+
+        private void 중급ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            label3.Text = File();
+            level = 20;
+        }
+
+        private void 고급ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            label3.Text = File();
+            level = 30;
+        }
+    }
+}
